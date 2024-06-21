492dbce1f9be1da6c19edeca7b6ee0bcba55a6d9
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2024-06-20 23:10:36 -0300

    Finalizing the documentation

--- jest.config.ts
+++ jest.config.ts
@@ -1,25 +1,25 @@
-import type { Config } from 'jest'
-import {pathsToModuleNameMapper} from 'ts-jest'
-import nextJest from 'next/jest.js'
-import { compilerOptions } from './tsconfig.json'
- 
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import nextJest from 'next/jest.js';
+import { compilerOptions } from './tsconfig.json';
+
 const createJestConfig = nextJest({
-  dir: './',
-})
- 
+  dir: './'
+});
+
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  transform:{
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
-  preset:'ts-jest',
+  preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transformIgnorePatterns: ['/node_modules/'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
-}
-export default createJestConfig(config)
-
-  
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  })
+};
+export default createJestConfig(config);
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -22,11 +22,7 @@ export default function Filter({ selectedValue, onChange }: IFilterProps) {
         ))}
       </select>
       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-        <svg
-          className="fill-current h-4 w-4"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-        >
+        <svg className="fill-current h-4 w-4" viewBox="0 0 20 20">
           <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
         </svg>
       </div>
